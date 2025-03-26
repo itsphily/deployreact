5e42dbaec9dfb11f3401ae815fa89eaae18e01cd
Phil habz <philippe.habra@gmail.com>
2025-03-26 06:47:30 -0400

    changed the agent to async

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -6,7 +6,7 @@ from langgraph.prebuilt import ToolNode
 
 
 @lru_cache(maxsize=4)
-def _get_model(model_name: str):
+async def _get_model(model_name: str):
     if model_name == "openai":
         model = ChatOpenAI(temperature=0, model_name="gpt-4o")
     elif model_name == "anthropic":
@@ -18,7 +18,7 @@ def _get_model(model_name: str):
     return model
 
 # Define the function that determines whether to continue or not
-def should_continue(state):
+async def should_continue(state):
     messages = state["messages"]
     last_message = messages[-1]
     # If there are no tool calls, then we finish
@@ -32,12 +32,12 @@ def should_continue(state):
 system_prompt = """Be a helpful assistant"""
 
 # Define the function that calls the model
-def call_model(state, config):
+async def call_model(state, config):
     messages = state["messages"]
     messages = [{"role": "system", "content": system_prompt}] + messages
     model_name = config.get('configurable', {}).get("model_name", "anthropic")
     model = _get_model(model_name)
-    response = model.invoke(messages)
+    response = await model.ainvoke(messages)
     # We return a list, because this will get added to the existing list
     return {"messages": [response]}
 
